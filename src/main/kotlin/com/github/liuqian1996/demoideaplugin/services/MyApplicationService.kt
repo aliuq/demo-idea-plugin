89eaa0c259d4fa7026ddc19472d6e23f01635433
GitHub Action <action@github.com>
2021-12-01 11:03:27 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/liuqian1996/demoideaplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.liuqian1996.demoideaplugin.services
+
+import com.github.liuqian1996.demoideaplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/liuqian1996/demoideaplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/liuqian1996/demoideaplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.liuqian1996.demoideaplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.liuqian1996.demoideaplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
